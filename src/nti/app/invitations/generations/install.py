b440b3d63bb8895e19f8ec73dc0c212b7f1a950c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-17 18:59:01 -0500

    add generations

---
+++ src/nti/app/invitations/generations/install.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import interface
+
+from zope.generations.generations import SchemaManager as BaseSchemaManager
+
+from zope.generations.interfaces import IInstallableSchemaManager
+
+from zope.intid.interfaces import IIntIds
+
+from nti.invitations.index import install_invitations_catalog
+
+generation = 1
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@interface.implementer(IInstallableSchemaManager)
+class _SchemaManager(BaseSchemaManager):
+    """
+    A schema manager that we can register as a utility in ZCML.
+    """
+
+    def __init__(self):
+        super(_SchemaManager, self).__init__(
+                generation=generation,
+                minimum_generation=generation,
+                package_name='nti.app.invitations.generations')
+
+    def install(self, context):
+        evolve(context)
+
+
+def evolve(context):
+    conn = context.connection
+    root = conn.root()
+    dataserver_folder = root['nti.dataserver']
+    lsm = dataserver_folder.getSiteManager()
+    intids = lsm.getUtility(IIntIds)
+    install_invitations_catalog(dataserver_folder, intids)
