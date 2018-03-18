789b1e8e33dc9894866cf1f6fd910621cb2b9ccb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-17 19:20:26 -0500

    clean up

--- src/nti/app/invitations/tests/test_admin_views.py
+++ src/nti/app/invitations/tests/test_admin_views.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904
+# pylint: disable=protected-access,too-many-public-methods,arguments-differ
 
 from hamcrest import is_
 from hamcrest import is_not
@@ -16,15 +16,15 @@ does_not = is_not
 
 from zope import component
 
-from nti.invitations.interfaces import IInvitationsContainer
-
-from nti.invitations.model import Invitation
-
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 
 from nti.dataserver.tests import mock_dataserver
+
+from nti.invitations.interfaces import IInvitationsContainer
+
+from nti.invitations.model import Invitation
 
 
 class TestAdminViews(ApplicationLayerTest):
