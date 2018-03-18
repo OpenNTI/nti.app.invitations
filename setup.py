67b98b6aeef2ef16fbd3478fd6453e06d7b7e353
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-17 21:46:51 -0500

    fix namespace

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    namespace_packages=['nti', 'app'],
+    namespace_packages=['nti', 'nti.app'],
     tests_require=TESTS_REQUIRE,
     install_requires=[
         'setuptools',
