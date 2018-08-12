80a3042b643ac9c04e0c13cbab4b1fe6f608bd9f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-11 19:12:31 -0500

    update headers

--- src/nti/app/invitations/logon.py
+++ src/nti/app/invitations/logon.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
+from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-from __future__ import division
 
 from zope import interface
 
@@ -12,8 +15,6 @@ from nti.app.invitations.interfaces import IChallengeLogonProvider
 from nti.schema.fieldproperty import createDirectFieldProperties
 
 from nti.schema.schema import SchemaConfigured
-
-__docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
